3b2b65f2b4628b2e5a67eb808fbe61e555b3189e
Rennish <rennishm@gmail.com>
2023-05-26 16:34:49 +0300

    first commit

---
+++ bef/name.php
@@ -0,0 +1,3 @@
+<?php
+    echo "rennish";
+?>/Users/kelsie/Programming/VSCODE/bef/name.php
---
+++ trial.java
@@ -0,0 +1,9 @@
+import java.lang.* ;
+
+class trial{
+
+    public static void main(String args[]){
+        System.out.println("my new page");
+    }
+}
+
